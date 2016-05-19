8ec6587e3547f7f5bce0a20f548130447e28f336
Rocka <Rocka@192.168.0.143>
2016-05-19 18:08:10 +0200

    Teszt

--- src/project/ProjectMain.java
+++ src/project/ProjectMain.java
@@ -17,6 +17,7 @@ public class ProjectMain {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("YOLO");
     }
     
 }
