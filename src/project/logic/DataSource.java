d6f2051cd3e396a31c383453b11799b86f4e2f12
Varga Bence <sprixz@mint3542>
2016-05-21 19:44:24 +0200

    DataSource osztály létrehozva.

--- src/project/logic/DataSource.java
+++ src/project/logic/DataSource.java
@@ -5,10 +5,55 @@
  */
 package project.logic;
 
+import javax.persistence.Cache;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import project.controllers.SubjectJpaController;
+import project.controllers.UserJpaController;
+
 /**
  *
  * @author Varga Bence
  */
 public class DataSource {
+
+    private static DataSource dataSource = null;
+    private EntityManagerFactory emf;
+    private final Cache cache = getEntityManagerFactory().getCache();
+
+    private SubjectJpaController subjectController;
+    private UserJpaController userController;
+
+    public static DataSource getInstance() {
+        if (dataSource == null) {
+            dataSource = new DataSource();
+
+        }
+        return dataSource;
+    }
+
+    public SubjectJpaController getSubjectController() {
+        return subjectController;
+    }
+
+    public UserJpaController getUserController() {
+        return userController;
+    }
     
+    
+
+    private DataSource() {
+        userController = new UserJpaController(getEntityManagerFactory());
+        subjectController = new SubjectJpaController(getEntityManagerFactory());
+    }
+
+    public final EntityManagerFactory getEntityManagerFactory() {
+        emf = Persistence.createEntityManagerFactory("ProjectPU");
+        return emf;
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
 }
